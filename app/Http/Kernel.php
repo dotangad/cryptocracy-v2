4ae9fd669d1c1e2d4779f6bd037f623458c75cee
Angad Singh <mail@angad.dev>
2021-06-22 18:58:46 +0530

    feat: admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'admin' => \App\Http\Middleware\EnsureAdmin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
