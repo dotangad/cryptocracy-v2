8144e833c213d127b0af77100611db1e8b6f8583
Angad Singh <mail@angad.dev>
2021-06-23 13:35:11 +0530

    feat: User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,10 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'username' => $this->faker->unique()->userName(),
+            'country' => 'IN',
+            'phone' => '+123 123 123',
+            'company' => $this->faker->company(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
