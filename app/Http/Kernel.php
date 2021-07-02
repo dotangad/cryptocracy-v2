52e9d17dd5e347000db5c73ab1a3d921646108f7
Angad Singh <mail@angad.dev>
2021-07-02 13:18:56 +0530

    feat: in progress middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,7 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'admin' => \App\Http\Middleware\EnsureAdmin::class,
         'dq' => \App\Http\Middleware\Disqualified::class,
+        'inprogress' => \App\Http\Middleware\InProgress::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
