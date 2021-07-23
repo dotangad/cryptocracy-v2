52765b30406f9c33f7b040d3029b29441e630988
Angad Singh <mail@angad.dev>
2021-07-23 12:21:04 +0530

    feat: custom error pages

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="icon" href="/img/footer-logo-white-bg.png" type="image/png" />
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/fonts/style.css') }}" rel="stylesheet" />
+    <title>Cryptocracy 2021</title>
+</head>
+
+<body>
+    <div className="layers">
+        <div class="layer center" style="z-index: 1000; display: flex; justify-content: center; align-items: center; flex-direction: column;">
+            <h1>500: Server Error</h1>
+            <a href="/">Home</a>
+        </div>
+    </div>
+</body>
+
+</html>
