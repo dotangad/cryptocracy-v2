222b2b5884fa4b7fc96c9d1221fbea8fdd9053a3
Angad Singh <mail@angad.dev>
2021-07-26 11:02:59 +0530

    fix: referral_code unique validation rule

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -11,7 +11,7 @@ class ReferralController extends Controller
     public function set_referral_code(Request $request)
     {
         $body = $request->validate([
-            'referral_code' => 'required|regex:/^[A-Z0-9]+$/'
+            'referral_code' => 'required|regex:/^[A-Z0-9]+$/|unique:users,referral_code'
         ]);
 
         User::where('id', auth()->id())
