a853de9f7b9c55b9158e9c527534ffae9db8ef57
Angad Singh <mail@angad.dev>
2021-07-27 10:52:32 +0530

    feat: refactor notifications out of IndexAuthenticated.tsx

---
+++ resources/js/Components/Notifications.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+
+const NotificationsContainer = styled.div`
+  min-height: 0;
+  overflow: hidden;
+  height: calc((100vh - 150px) * 3 / 4);
+  width: 100%;
+
+  & > div {
+    height: calc(100%);
+    overflow: auto;
+  }
+
+  & > div > div {
+    margin: 20px;
+    padding: 10px 15px;
+    background: #292929;
+    display: flex;
+    flex-direction: column;
+    font-size: 0.9rem;
+
+    > span.timestamp {
+      width: 100%;
+      text-align: right;
+      font-weight: bold;
+      color: #888;
+      font-size: 0.8rem;
+      padding: 0 5px;
+    }
+  }
+`;
+
+interface INotificationsProps {
+  notifications: { content: string; created_at: string }[];
+}
+
+const Notifications: React.FC<INotificationsProps> = ({
+  notifications
+}: INotificationsProps) => {
+  return (
+    <NotificationsContainer>
+      <div>
+        {notifications.length === 0 ? (
+          <div>
+            <span style={{ fontWeight: "bold", color: "#999" }}>
+              No new notifications
+            </span>
+          </div>
+        ) : (
+          notifications.map(({ content, created_at }, i) => (
+            <div key={i}>
+              <span>{content}</span>
+              <span className="timestamp">{created_at}</span>
+            </div>
+          ))
+        )}
+      </div>
+    </NotificationsContainer>
+  );
+};
+
+export default Notifications;
--- resources/js/Pages/IndexAuthenticated.tsx
+++ resources/js/Pages/IndexAuthenticated.tsx
@@ -7,6 +7,7 @@ import { ChevronRight } from "../Components/Icons";
 import Countdown from "../Components/Layout/Countdown";
 import countries from "../Data/countries";
 import { ReferralCodeForm } from "../Components/ReferralCodeForm";
+import Notifications from "../Components/Notifications";
 
 const Global = createGlobalStyle`
   .Content > div {
@@ -19,65 +20,6 @@ const Global = createGlobalStyle`
     align-items: flex-end;
   }
 `;
-
-const NotificationsContainer = styled.div`
-  min-height: 0;
-  overflow: hidden;
-  height: calc((100vh - 150px) * 3 / 4);
-  width: 100%;
-
-  & > div {
-    height: calc(100%);
-    overflow: auto;
-  }
-
-  & > div > div {
-    margin: 20px;
-    padding: 10px 15px;
-    background: #292929;
-    display: flex;
-    flex-direction: column;
-    font-size: 0.9rem;
-
-    > span.timestamp {
-      width: 100%;
-      text-align: right;
-      font-weight: bold;
-      color: #888;
-      font-size: 0.8rem;
-      padding: 0 5px;
-    }
-  }
-`;
-
-interface INotificationsProps {
-  notifications: { content: string; created_at: string }[];
-}
-
-const Notifications: React.FC<INotificationsProps> = ({
-  notifications
-}: INotificationsProps) => {
-  return (
-    <NotificationsContainer>
-      <div>
-        {notifications.length === 0 ? (
-          <div>
-            <span style={{ fontWeight: "bold", color: "#999" }}>
-              No new notifications
-            </span>
-          </div>
-        ) : (
-          notifications.map(({ content, created_at }, i) => (
-            <div key={i}>
-              <span>{content}</span>
-              <span className="timestamp">{created_at}</span>
-            </div>
-          ))
-        )}
-      </div>
-    </NotificationsContainer>
-  );
-};
 
 const SplitContainer = styled.div`
   width: 100%;
@@ -187,24 +129,23 @@ const UserCard: React.FC<any> = ({ referred_users }: any) => {
     .concat(
       auth.user.discord_discriminator && auth.user.discord_username
         ? [
-            {
-              label: "Discord",
-              data:
-                "@" + auth.user.discord_username + "#" + auth.user.discord_discriminator
-            }
-          ]
+          {
+            label: "Discord",
+            data:
+              "@" + auth.user.discord_username + "#" + auth.user.discord_discriminator
+          }
+        ]
         : []
     )
     .concat(
       auth.user.referral_code
         ? [
-            {
-              label: "Referral Code",
-              data: `${auth.user.referral_code} (Referred ${referred_users} user${
-                referred_users === 1 ? "" : "s"
+          {
+            label: "Referral Code",
+            data: `${auth.user.referral_code} (Referred ${referred_users} user${referred_users === 1 ? "" : "s"
               })`
-            }
-          ]
+          }
+        ]
         : []
     );
 
