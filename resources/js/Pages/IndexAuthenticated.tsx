06447b27f4d016ec800b747a43bc1079a40f2eec
Angad Singh <mail@angad.dev>
2021-07-27 11:11:03 +0530

    fix: index authenticated overflow bug

--- resources/js/Pages/IndexAuthenticated.tsx
+++ resources/js/Pages/IndexAuthenticated.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import { InertiaLink, usePage } from "@inertiajs/inertia-react";
 import styled, { createGlobalStyle } from "styled-components";
 import Layout, { SidebarTop, IPageProps } from "../Components/Layout";
-import { PrimaryButton } from "../Components/Button";
 import { ChevronRight } from "../Components/Icons";
 import Countdown from "../Components/Layout/Countdown";
-import countries from "../Data/countries";
 import { ReferralCodeForm } from "../Components/ReferralCodeForm";
 import Notifications from "../Components/Notifications";
+import UserCard, { UserCardContainer } from "../Components/IndexUserCard";
 
 const Global = createGlobalStyle`
   .Content > div {
@@ -31,13 +30,19 @@ const SplitContainer = styled.div`
   flex-wrap: wrap;
 
   & > div {
-    height: 100%;
+    height: calc(100%);
+    overflow: auto;
     width: 50%;
     min-width: 300px;
     padding: 20px;
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
+
+    & > div {
+      height: calc(100%);
+      overflow: auto;
+    }
   }
 
   @media screen and (max-width: 900px) {
@@ -68,110 +73,6 @@ const BigContainer = styled.div`
   }
 `;
 
-const UserCardContainer = styled.div`
-  padding: 30px;
-  background: #292929;
-  margin: 10px 0;
-
-  .buttons {
-    display: flex;
-    justify-content: "space-between";
-    width: "100%";
-  }
-
-  @media screen and (max-width: 600px) {
-    .buttons {
-      flex-direction: column;
-      align-items: center;
-      a {
-        width: 100%;
-        margin: 10px 0;
-      }
-    }
-  }
-`;
-
-const UserCreatedAt = styled.div`
-  font-weight: bold;
-  color: #888;
-  font-size: 0.9rem;
-`;
-
-const UserAttribute = styled.div`
-  margin: 20px 0;
-
-  & > div:first-child {
-    font-size: 0.9rem;
-    font-weight: bold;
-    color: #888;
-  }
-
-  & > div:last-child {
-    font-size: 1.1rem;
-  }
-`;
-
-const UserCard: React.FC<any> = ({ referred_users }: any) => {
-  const { auth } = usePage<IPageProps>().props;
-
-  const country = Object.entries(countries).find(([iso]) => iso === auth.user.country);
-  const userData = [
-    { label: "Name", data: auth.user.name },
-    { label: "Email", data: auth.user.email },
-    { label: "Institution / Company", data: auth.user.company },
-    { label: "Phone", data: auth.user.phone },
-    {
-      label: "Country",
-      // @ts-ignore
-      data: country ? country[1] : "Unknown"
-    }
-  ]
-    .concat(
-      auth.user.discord_discriminator && auth.user.discord_username
-        ? [
-          {
-            label: "Discord",
-            data:
-              "@" + auth.user.discord_username + "#" + auth.user.discord_discriminator
-          }
-        ]
-        : []
-    )
-    .concat(
-      auth.user.referral_code
-        ? [
-          {
-            label: "Referral Code",
-            data: `${auth.user.referral_code} (Referred ${referred_users} user${referred_users === 1 ? "" : "s"
-              })`
-          }
-        ]
-        : []
-    );
-
-  return (
-    <UserCardContainer>
-      <h1 style={{ fontSize: "1.6rem" }}>@{auth.user.username}</h1>
-      <UserCreatedAt>Registered {auth.user.created}</UserCreatedAt>
-      {userData.map(({ label, data }, i) => (
-        <UserAttribute key={i}>
-          <div>{label}</div>
-          <div>{data}</div>
-        </UserAttribute>
-      ))}
-      {!auth.user.discord_username && !auth.user.discord_discriminator && (
-        <div
-          style={{ display: "flex", justifyContent: "space-between", width: "100%" }}
-          className="buttons"
-        >
-          <PrimaryButton style={{ padding: "10px 20px" }} href="/connectdiscord">
-            Connect Discord
-          </PrimaryButton>
-        </div>
-      )}
-    </UserCardContainer>
-  );
-};
 
 const NumberCard = styled.div`
   width: 100%;
@@ -265,44 +166,46 @@ const Index: React.FC<IIndexProps> = ({ notifications, referred_users }: IIndexP
               <UserCard referred_users={referred_users} />
             </div>
             <div>
-              {started ? (
-                <>
-                  {!ended && (
-                    <>
-                      <div>
-                        <Countdown large={true} />
-                      </div>
-                      {!user.referral_code && <ReferralCodeForm />}
-                      <NumberCard>
-                        <span>{user.tile_id}</span>
-                        <div>tile</div>
-                      </NumberCard>
-                    </>
-                  )}
-                  <NumberCard>
-                    <span>{user.points}</span>
-                    <div>points</div>
-                  </NumberCard>
-                  {ended ? (
-                    <InfoContainer>Cryptocracy 2021 has ended.</InfoContainer>
-                  ) : (
-                    <PlayBtn href="/play">
-                      <span>Play</span>
-                      <ChevronRight />
-                    </PlayBtn>
-                  )}
-                </>
-              ) : !ended ? (
-                <>
-                  <div>
-                    <Countdown large={true} />
-                  </div>
-                  {!user.referral_code && <ReferralCodeForm />}
-                  <InfoContainer>Cryptocracy 2021 has not started yet.</InfoContainer>
-                </>
-              ) : (
-                <></>
-              )}
+              <div>
+                {started ? (
+                  <>
+                    {!ended && (
+                      <>
+                        <div>
+                          <Countdown large={true} />
+                        </div>
+                        {!user.referral_code && <ReferralCodeForm />}
+                        <NumberCard>
+                          <span>{user.tile_id}</span>
+                          <div>tile</div>
+                        </NumberCard>
+                      </>
+                    )}
+                    <NumberCard>
+                      <span>{user.points}</span>
+                      <div>points</div>
+                    </NumberCard>
+                    {ended ? (
+                      <InfoContainer>Cryptocracy 2021 has ended.</InfoContainer>
+                    ) : (
+                      <PlayBtn href="/play">
+                        <span>Play</span>
+                        <ChevronRight />
+                      </PlayBtn>
+                    )}
+                  </>
+                ) : !ended ? (
+                  <>
+                    <div>
+                      <Countdown large={true} />
+                    </div>
+                    {!user.referral_code && <ReferralCodeForm />}
+                    <InfoContainer>Cryptocracy 2021 has not started yet.</InfoContainer>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
             </div>
           </SplitContainer>
         </BigContainer>
