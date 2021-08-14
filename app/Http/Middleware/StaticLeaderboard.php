f0df58138817cd8344a97911fbd033d7a2a1ea83
Angad Singh <mail@angad.dev>
2021-08-14 12:55:40 +0530

    fix: allow login

--- app/Http/Middleware/StaticLeaderboard.php
+++ app/Http/Middleware/StaticLeaderboard.php
@@ -9,24 +9,25 @@ use Illuminate\Support\Facades\Route;
 
 class StaticLeaderboard
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        if (
-            preg_match("/admin/", $request->path()) ||
-            preg_match("/leaderboard\/static/", $request->path()) ||
-            !\Carbon\Carbon::parse(env('END_TIME'))
-                ->lt(\Carbon\Carbon::now('Asia/Kolkata'))
-        ) {
-            return $next($request);
-        }
-
-        return Redirect::route('leaderboard.static');
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle(Request $request, Closure $next)
+  {
+    if (
+      preg_match("/admin/", $request->path()) ||
+      preg_match("/login", $request->path()) ||
+      preg_match("/leaderboard\/static/", $request->path()) ||
+      !\Carbon\Carbon::parse(env('END_TIME'))
+        ->lt(\Carbon\Carbon::now('Asia/Kolkata'))
+    ) {
+      return $next($request);
     }
+
+    return Redirect::route('leaderboard.static');
+  }
 }
